8ea509de2ce8d2e176a61d5c06b889ab47558298
Tomas Benjamin Vasquez <58223692+tomas0011@users.noreply.github.com>
2023-05-23 04:00:39 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Aprende react
           </a>
         </p>
       </header>
